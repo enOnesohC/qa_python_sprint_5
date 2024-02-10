91b45a06d563ef12e834068f367c0b3a5d614044
Andrey Ermolaev <enonesohc@mail.ru>
2024-02-10 16:22:26 +0300

    add

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -5,99 +5,96 @@
 начинки
 """
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 import time
 import locators
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def test_transition_bread():
-    browser = webdriver.Chrome()
-    browser.get(locators.authorization_link_login)
 
-    time.sleep(2)
-    input1 = browser.find_element(By.XPATH, locators.authorization_email)
-    input1.send_keys("enonesohc@ya.ru")
+class TestConstructor:
 
-    time.sleep(2)
-    input2 = browser.find_element(By.XPATH, locators.authorization_password)
-    input2.send_keys("xCCuHfmBJyqV3Fv")
+    def test_transition_bread(self):
+        browser = webdriver.Chrome()
+        browser.get(locators.authorization_link_login)
 
-    time.sleep(2)
-    button = browser.find_element(By.CLASS_NAME, locators.authorization_login_button)
-    button.click()
+        WebDriverWait(browser, 2).until(EC.visibility_of_element_located((By.XPATH, locators.authorization_email)))
+        input1 = browser.find_element(By.XPATH, locators.authorization_email)
+        input1.send_keys("enonesohc@ya.ru")
 
-    time.sleep(2)
-    span = WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.XPATH, locators.constructor_bread_xpath)))
+        WebDriverWait(browser, 2).until(EC.visibility_of_element_located((By.XPATH, locators.authorization_password)))
+        input2 = browser.find_element(By.XPATH, locators.authorization_password)
+        input2.send_keys("xCCuHfmBJyqV3Fv")
 
-    tab = span.find_element(By.XPATH, "./..")
+        WebDriverWait(browser, 2).until(EC.visibility_of_element_located((By.CLASS_NAME, locators.authorization_login_button)))
+        button = browser.find_element(By.CLASS_NAME, locators.authorization_login_button)
+        button.click()
 
-    browser.execute_script("arguments[0].click();", span)
+        WebDriverWait(browser, 2).until(EC.visibility_of_element_located((By.XPATH, locators.constructor_bread_xpath)))
+        span = WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.XPATH, locators.constructor_bread_xpath)))
 
-    time.sleep(2)
-    tab_class = tab.get_attribute("class")
+        tab = span.find_element(By.XPATH, "./..")
 
-    time.sleep(2)
-    assert tab_class == locators.constructor_current_class
-    browser.quit()
+        browser.execute_script("arguments[0].click();", span)
 
-def test_transition_sause():
-    browser = webdriver.Chrome()
-    browser.get(locators.authorization_link_login)
+        time.sleep(2)
+        tab_class = tab.get_attribute("class")
+        browser.quit()
 
-    time.sleep(2)
-    input1 = browser.find_element(By.XPATH, locators.authorization_email)
-    input1.send_keys("enonesohc@ya.ru")
+        assert tab_class == locators.constructor_current_class
 
-    time.sleep(2)
-    input2 = browser.find_element(By.XPATH, locators.authorization_password)
-    input2.send_keys("xCCuHfmBJyqV3Fv")
+    def test_transition_sause(self):
+        browser = webdriver.Chrome()
+        browser.get(locators.authorization_link_login)
 
-    time.sleep(2)
-    button = browser.find_element(By.CLASS_NAME, locators.authorization_login_button)
-    button.click()
+        WebDriverWait(browser, 2).until(EC.visibility_of_element_located((By.XPATH, locators.authorization_email)))
+        input1 = browser.find_element(By.XPATH, locators.authorization_email)
+        input1.send_keys("enonesohc@ya.ru")
 
-    time.sleep(2)
-    span = WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.XPATH, locators.constructor_sause_xpath)))
+        WebDriverWait(browser, 2).until(EC.visibility_of_element_located((By.XPATH, locators.authorization_password)))
+        input2 = browser.find_element(By.XPATH, locators.authorization_password)
+        input2.send_keys("xCCuHfmBJyqV3Fv")
 
-    tab = span.find_element(By.XPATH, "./..")
+        WebDriverWait(browser, 2).until(EC.visibility_of_element_located((By.CLASS_NAME, locators.authorization_login_button)))
+        button = browser.find_element(By.CLASS_NAME, locators.authorization_login_button)
+        button.click()
 
-    browser.execute_script("arguments[0].click();", span)
+        WebDriverWait(browser, 2).until(EC.visibility_of_element_located((By.XPATH, locators.constructor_sause_xpath)))
+        span = WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.XPATH, locators.constructor_sause_xpath)))
 
-    time.sleep(2)
-    tab_class = tab.get_attribute("class")
+        tab = span.find_element(By.XPATH, "./..")
 
-    time.sleep(2)
-    assert tab_class == locators.constructor_current_class
-    browser.quit()
+        browser.execute_script("arguments[0].click();", span)
 
+        tab_class = tab.get_attribute("class")
+        browser.quit()
+        assert tab_class == locators.constructor_current_class
 
-def test_transition_filling():
-    browser = webdriver.Chrome()
-    browser.get(locators.authorization_link_login)
+    def test_transition_filling(self):
+        browser = webdriver.Chrome()
+        browser.get(locators.authorization_link_login)
 
-    time.sleep(2)
-    input1 = browser.find_element(By.XPATH, locators.authorization_email)
-    input1.send_keys("enonesohc@ya.ru")
+        WebDriverWait(browser, 2).until(EC.visibility_of_element_located((By.XPATH, locators.authorization_email)))
+        input1 = browser.find_element(By.XPATH, locators.authorization_email)
+        input1.send_keys("enonesohc@ya.ru")
 
-    time.sleep(2)
-    input2 = browser.find_element(By.XPATH, locators.authorization_password)
-    input2.send_keys("xCCuHfmBJyqV3Fv")
+        WebDriverWait(browser, 2).until(EC.visibility_of_element_located((By.XPATH, locators.authorization_password)))
+        input2 = browser.find_element(By.XPATH, locators.authorization_password)
+        input2.send_keys("xCCuHfmBJyqV3Fv")
 
-    time.sleep(2)
-    button = browser.find_element(By.CLASS_NAME, locators.authorization_login_button)
-    button.click()
+        WebDriverWait(browser, 2).until(EC.visibility_of_element_located((By.CLASS_NAME, locators.authorization_login_button)))
+        button = browser.find_element(By.CLASS_NAME, locators.authorization_login_button)
+        button.click()
 
-    time.sleep(2)
-    span = WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.XPATH, locators.constructor_filling_xpath)))
+        WebDriverWait(browser, 2).until(EC.visibility_of_element_located((By.XPATH, locators.constructor_filling_xpath)))
+        span = WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.XPATH, locators.constructor_filling_xpath)))
 
-    tab = span.find_element(By.XPATH, "./..")
+        tab = span.find_element(By.XPATH, "./..")
 
-    browser.execute_script("arguments[0].click();", span)
+        browser.execute_script("arguments[0].click();", span)
 
-    time.sleep(2)
-    tab_class = tab.get_attribute("class")
+        tab_class = tab.get_attribute("class")
 
-    time.sleep(2)
-    assert tab_class == locators.constructor_current_class
-    browser.quit()
+        browser.quit()
+
+        assert tab_class == locators.constructor_current_class
