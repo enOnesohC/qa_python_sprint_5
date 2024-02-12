b47372618d98e470174f87a314c2d7e62a3557a7
Andrey Ermolaev <enonesohc@mail.ru>
2024-02-12 08:18:35 +0300

    Правка замечаний_1

---
+++ tests/urls.py
@@ -0,0 +1,8 @@
+"""links"""
+
+#authorization
+authorization_link_login = "https://stellarburgers.nomoreparties.site/login"
+authorization_link_main = "https://stellarburgers.nomoreparties.site/"
+
+#registration
+registration_link = "https://stellarburgers.nomoreparties.site/register"
